955701f22bca5b2d37fd688bf03f330d43f47cc6
Glenn Cole <gcole@mac.com>
2018-10-28 21:34:35 -0700

    Initial Commit

---
+++ Mac_Project9_GCD/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Mac_Project9_GCD
+//
+//  Created by Glenn Cole on 10/28/18.
+//  Copyright © 2018 Glenn Cole. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Mac_Project9_GCD/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Mac_Project9_GCD
+//
+//  Created by Glenn Cole on 10/28/18.
+//  Copyright © 2018 Glenn Cole. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
